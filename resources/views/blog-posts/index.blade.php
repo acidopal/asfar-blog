60615c7e9014130418d714e27307869723d1318e
acidopal <im.acidopal@gmail.com>
2024-06-15 08:18:00 +0700

    feat: add permision on blog post

--- resources/views/blog-posts/index.blade.php
+++ resources/views/blog-posts/index.blade.php
@@ -47,17 +47,21 @@
                                             <td class="px-6 py-3 border border-gray-300">{{ $blogPost->user->name }}</td>
                                             @canany(['blog-post-edit', 'blog-post-delete'])
                                             <td class="px-6 py-3 border border-gray-300">
-                                                <form onsubmit="return confirm('Are you sure ?');" action="{{ route('blog-posts.destroy', $blogPost->id) }}" method="POST">
+                                                @can('blog-post-edit')
                                                     <a href="{{ route('blog-posts.edit', $blogPost->id) }}" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
                                                             Edit
                                                     </a>
+                                                @endcan
 
-                                                    @csrf
-                                                    @method('DELETE')
-                                                    <button type="submit" class="inline-flex items-center px-4 py-2 bg-red-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-500 active:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 transition ease-in-out duration-150">
-                                                        Delete
-                                                    </button>
-                                                </form>
+                                                @can('blog-post-delete')
+                                                    <form onsubmit="return confirm('Are you sure ?');" action="{{ route('blog-posts.destroy', $blogPost->id) }}" method="POST">
+                                                        @csrf
+                                                        @method('DELETE')
+                                                        <button type="submit" class="inline-flex items-center px-4 py-2 bg-red-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-500 active:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 transition ease-in-out duration-150">
+                                                            Delete
+                                                        </button>
+                                                    </form>
+                                                @endcan
                                             </td>
                                             @endcan
                                         </tr>
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -44,22 +44,21 @@
                                                 </td>
                                                 @canany(['user-edit', 'user-delete'])
                                                 <td class="px-6 py-3 border border-gray-300">
-                                                    <form onsubmit="return confirm('Are you sure ?');" action="{{ route('users.destroy', $user->id) }}" method="POST">
-                                                        @can('user-edit')
+                                                    @can('user-edit')
                                                         <a href="{{ route('users.edit', $user->id) }}" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
                                                             Edit
                                                         </a>
-                                                        @endcan
+                                                    @endcan
 
-                                                        @csrf
-                                                        @method('DELETE')
-
-                                                        @can('user-delete')
-                                                        <button type="submit" class="inline-flex items-center px-4 py-2 bg-red-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-500 active:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 transition ease-in-out duration-150">
-                                                            Delete User
-                                                        </button>
-                                                        @endcan
-                                                    </form>
+                                                    @can('user-delete')
+                                                        <form onsubmit="return confirm('Are you sure ?');" action="{{ route('users.destroy', $user->id) }}" method="POST">
+                                                            @csrf
+                                                            @method('DELETE')
+                                                            <button type="submit" class="inline-flex items-center px-4 py-2 bg-red-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-500 active:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 transition ease-in-out duration-150">
+                                                                Delete User
+                                                            </button>
+                                                        </form>
+                                                    @endcan
                                                 </td>
                                                 @endcan
                                             </tr>
